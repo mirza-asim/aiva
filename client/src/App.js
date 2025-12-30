e0c2a7ac9b265ea2ca1393cde45a8731bdbaf9c1
Mirza <asimbaig1999@yahoo.com>
2025-12-29 21:58:40 -0500

    aiva launch 2.1

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
     
    try {
 
-    const res = await axios.post('http://localhost:8000/ask', {inputValue})
+    const res = await axios.post('https://aiva-server.vercel.app/ask', {inputValue})
     .then((res)=>res.data)
     .then((finalRes)=>{
       console.log("FINAL-RES = ", finalRes);
